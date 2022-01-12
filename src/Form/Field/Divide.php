f7c0ba7390f50e0fbaf5f52caed0d1318c3888bd
yonghaodg <1179990227@qq.com>
2022-01-12 16:23:04 +0800

    修改form的divide

--- src/Form/Field/Divide.php
+++ src/Form/Field/Divide.php
@@ -6,6 +6,7 @@ use Dcat\Admin\Form\Field;
 
 class Divide extends Field
 {
+
     public function __construct($label = null)
     {
         $this->label = $label;
@@ -13,13 +14,20 @@ class Divide extends Field
 
     public function render()
     {
+        $attributes = '';
+
+        foreach ($this->attributes as $k=>$v){
+            $attributes = $attributes.' '.$k.'='.$v.' ';
+        }
         if (! $this->label) {
             return '<hr/>';
         }
 
         return <<<HTML
-<div class="mt-2 text-center mb-2 form-divider">
-  <span>{$this->label}</span>
+<div class="mt-2 text-center mb-2 form-divider" $attributes>
+  <span class="pos-relative label-close">{$this->label}
+  <span class="triangle-icon"></span></span>
+
 </div>
 HTML;
     }
